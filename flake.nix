d4cc5c2773b4c035b96d2911a5199f328a15bd5a
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:29 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "React example application demonstrating Akeyless OIDC identity provider integration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.default = pkgs.buildNpmPackage {
+        pname = "akeyless-oidc-identity-provider-example";
+        version = "0.0.0-dev";
+        src = self;
+        npmDepsHash = "sha256-IBqZaU2SiOdXxPclbx+C4MLjSrUBl0TGCguBf6YORRU="; # TODO: set correct hash
+        dontNpmBuild = false;
+        npmBuildScript = "build";
+        meta = {
+          description = "React example application demonstrating Akeyless OIDC identity provider integration";
+          homepage = "https://github.com/pleme-io/akeyless-oidc-identity-provider-example";
+          license = pkgs.lib.licenses.mit;
+        };
+      };
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ nodejs_22 ];
+      };
+    });
+}
