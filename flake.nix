fa9ac3e45b61c21481d9d81ed082e131cbfd0672
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:30 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,20 @@
 {
   description = "React example application demonstrating Akeyless OIDC identity provider integration";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.default = pkgs.buildNpmPackage {
-        pname = "akeyless-oidc-identity-provider-example";
-        version = "0.0.0-dev";
-        src = self;
-        npmDepsHash = "sha256-IBqZaU2SiOdXxPclbx+C4MLjSrUBl0TGCguBf6YORRU=";
-        dontNpmBuild = false;
-        npmBuildScript = "build";
-        meta = {
-          description = "React example application demonstrating Akeyless OIDC identity provider integration";
-          homepage = "https://github.com/pleme-io/akeyless-oidc-identity-provider-example";
-          license = pkgs.lib.licenses.mit;
-        };
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ nodejs_22 ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "npm";
+    builder = "package";
+    pname = "akeyless-oidc-identity-provider-example";
+    npmDepsHash = "sha256-IBqZaU2SiOdXxPclbx+C4MLjSrUBl0TGCguBf6YORRU=";
+    dontNpmBuild = false;
+    npmBuildScript = "build";
+    description = "React example application demonstrating Akeyless OIDC identity provider integration";
+  };
 }
